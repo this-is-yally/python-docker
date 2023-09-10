658c08311e5fd9015e6cb7eeb594f05f772d9282
Mykhailo Filatov <79525614+this-is-yally@users.noreply.github.com>
2023-09-10 20:07:43 +0200

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # Build the Docker image
-docker build -t flask-example .
+docker build -t flask-app .
 
 # Run a container from the built image
-docker run -d -p 5000:5000 --name flask-app flask-example
+docker run -d -p 5000:5000 --name flask-container flask-app
 
-# Wait for the Flask app to start
+# Wait for the Flask app to start (you can add a more sophisticated check here)
 sleep 5
 
 # Send a request to the app and store the response
 response=$(curl -s http://localhost:5000)
 
 # Check if the response contains the expected message
-if [[ $response == *"Hello, my dear viewer/recruiter! It is just an example on Flask for Docker"* ]]; then
+if [[ $response == *"This is a more complex Flask example."* ]]; then
   echo "Dockerfile test passed: Expected message found."
   exit 0
 else
@@ -22,5 +22,5 @@ else
 fi
 
 # Cleanup - stop and remove the test container
-docker stop flask-app
-docker rm flask-app
+docker stop flask-container
+docker rm flask-container
